044ce2d9eab5ae36824c9de3bddf7b24b26d490f
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-01-10 22:46:47 +0900

    ㅇㅇ

--- Assets/02_Scripts/Dev/Junho/Pattern/PatternCount/Pattern3.cs
+++ Assets/02_Scripts/Dev/Junho/Pattern/PatternCount/Pattern3.cs
@@ -7,6 +7,7 @@ using UnityEditor.Tilemaps;
 public class Pattern3 : MonoBehaviour
 {
     public GameObject _bullet;
+    public GameObject _warningArea;
     
     public GameObject leftup;
     public GameObject leftmiddle;
@@ -18,7 +19,7 @@ public class Pattern3 : MonoBehaviour
     public GameObject middledown;
 
     private Pattern2 _p2;
-    private bool _isBounce = false;
+    private bool _isBounce = true;
 
     private void Awake()
     {
@@ -36,9 +37,6 @@ public class Pattern3 : MonoBehaviour
         {
             Pattern3Process();
         }
-
-
-        //GameObject a = PoolManager.Instance.Pop(_bullet, transform.position, Quaternion.identity);
     }
 
     private void Pattern3Process()
@@ -50,29 +48,47 @@ public class Pattern3 : MonoBehaviour
             seq.AppendCallback(() => _isBounce = true);
             seq.AppendCallback(RotateChrome);
             seq.AppendInterval(3.5f);
-            seq.AppendCallback(() => _isBounce = false);
-            seq.Append(transform.DOScale(new Vector3(0,0,0), 0.5f));
+            //seq.AppendCallback(() => _isBounce = false);
+            //seq.Append(transform.DOScale(new Vector3(0,0,0), 0.5f));
         }
     }
 
-    private void RotateChrome()
+    private void RotateChrome() // 여기 고치기
     {
         Sequence seq = DOTween.Sequence();
 
         for (int i = 0; i < 7; i++)
         {
-            int rand = Random.Range(0, 361);
+            int rand = Random.Range(-90, 90);
 
             seq.Append(transform.DORotate(new Vector3(0, 0, rand), 0.42f));
-            seq.AppendCallback(BulletFire);
+            seq.AppendCallback(() => StartCoroutine(CreateWarning(transform.position, new Vector2(55, 30), Vector3.zero, 0.3f)));
+
+            seq.AppendInterval(1f);
+
+            for (int j = 0; j < 5; j++)
+            {
+                seq.AppendCallback(() => BulletFire(leftup.transform, (leftup.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(leftmiddle.transform, (leftmiddle.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(leftdown.transform, (leftdown.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(rightup.transform, (rightup.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(rightmiddle.transform, (rightmiddle.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(rightdown.transform, (rightdown.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(middleup.transform, (middleup.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendCallback(() => BulletFire(middledown.transform, (middledown.transform.rotation.eulerAngles.z - transform.rotation.eulerAngles.z)*-1));
+                seq.AppendInterval(0.2f);
+
+            }
+
+            seq.AppendInterval(1f);
         }
         seq.Append(transform.DORotate(Vector3.zero, 0.42f));
     }
 
-    private void BulletFire()
+    private void BulletFire(Transform position, float rotation)
     {
-        Quaternion dir = Quaternion.Euler(new Vector3(0,0,0));
-        PoolManager.Instance.Pop(_bullet, transform.position, dir);
+        GameObject a = PoolManager.Instance.Pop(_bullet, position.position, Quaternion.Euler(0, 0, rotation));
+        print(a.transform.rotation);
     }
 
     IEnumerator ChromeBounce()
@@ -84,5 +100,15 @@ public class Pattern3 : MonoBehaviour
             transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0.5f);
             yield return new WaitForSeconds(0.21f);
         }
+    }
+
+    IEnumerator CreateWarning(Vector2 createPos, Vector2 scale, Vector3 rotation, float delay)
+    {
+        GameObject a = PoolManager.Instance.Pop(_warningArea, createPos, Quaternion.identity);
+        a.transform.localScale = scale;
+        a.transform.rotation = Quaternion.Euler(rotation);
+        
+        yield return new WaitForSeconds(delay);
+        PoolManager.Instance.Push(a);
     }
 }
