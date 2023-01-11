1bee046d9821154ea07284d64de53576fcde499f
June060522 <a80336234@gmail.com>
2023-01-12 00:35:46 +0900

    IntroCompelete

---
+++ Assets/02_Scripts/Dev/JunE/BlackSkinPrice.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class BlackSkinPrice : MonoBehaviour
+{
+    public int price = 0;
+    public TextMeshProUGUI priceTxt;
+    [SerializeField] int index;
+    [SerializeField] TextMeshProUGUI TribeKae;
+    [SerializeField] AudioSource selectSource;
+
+    private void OnEnable()
+    {
+        SelectManager.Instance.UpdateBool();
+    }
+
+    public void UpdateBlackPrice()
+    {
+        price = 0;
+        for(int i = 0; i < index; i++)
+        {
+            if(!SelectManager.Instance.IsBuy(i))
+                price += 200;
+        }
+    }
+
+    public void OnClickImage()
+    {
+        if (SelectManager.Instance.IsBuy(index))
+        {
+            PlayerPrefs.SetInt("ColorIndex", index);
+            selectSource.Play();
+            SelectManager.Instance.UpdateBool();
+        }
+        else
+        {
+            if (price == 0)
+            {
+                BuyClip.Instance.PlaySound();
+                SelectManager.Instance.UpdateKae();
+                PlayerPrefs.SetString($"{index}", "삼");
+                SelectManager.Instance.UpdateBool();
+            }
+            else
+            {
+                if (!TribeKae.gameObject.activeSelf)
+                    StartCoroutine(DofadeText(TribeKae));
+                SkinSound.Instance.PlaySound();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("리셋~~~~~~");
+            PlayerPrefs.SetString($"{index}", "사야댐");
+            SelectManager.Instance.UpdateBool();
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            PlayerPrefs.SetInt("KaeJewel", PlayerPrefs.GetInt("KaeJewel", 0) + 200);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            PlayerPrefs.SetInt("KaeJewel",0);
+        }
+        UpdateBlackPrice();
+        if(!SelectManager.Instance.IsBuy(index))
+        {
+            priceTxt.text = $"{price}캐";
+        }
+    }
+
+    IEnumerator DofadeText(TextMeshProUGUI text)
+    {
+        text.gameObject.SetActive(true);
+        Sequence seq = DOTween.Sequence();
+        seq.Append(text.DOFade(1, 0.6f));
+        seq.Append(text.DOFade(0, 0.6f));
+        yield return new WaitForSeconds(1.2f);
+        text.gameObject.SetActive(false);
+    }
+}
